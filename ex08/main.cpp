5c6eaaa8db152a79846d3b435f47c537cb055caa
Ambroise Bareux <ambroise.bareux@gmail.com>
2024-03-14 10:56:39 +0100

    added test ex08

--- ex08/main.cpp
+++ ex08/main.cpp
@@ -8,4 +8,8 @@ auto main() -> int {
     matrix_a = Matrix<int>(3, 3, {-2, -8, 4, 1, -23, 4, 0, 6, 4});
 
     std::cout << matrix_a.trace() << std::endl;
+
+    matrix_a = Matrix<int>(3, 3, {1, 0, 0, 0, 1, 0, 0, 0, 1});
+
+    std::cout << matrix_a.trace() << std::endl;
 }
